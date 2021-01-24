88a8418efef96f9b484172f24b0fc65c0e086c5c
vikas yadav <vikas626639@gmail.com>
2021-01-24 20:41:16 +0530

    crud operation

--- proj04-Contect_master_app/src/main/java/com/nit/Application.java
+++ proj04-Contect_master_app/src/main/java/com/nit/Application.java
@@ -13,13 +13,10 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 		ContectMaster bean = run.getBean(ContectMaster.class);	//contect master is a interface internally implementation class object got created by proxy object
-		
-		//create entity class object
 		Contect_master entity=new Contect_master();
-		entity.setContect_name("vikas");
-		entity.setContect_number(8871662081l);
-		bean.save(entity);
-		System.out.println("Record saved");
+		  entity.setContect_name("vikash");
+		   entity.setContect_number(9630550198L);
+		  bean.save(entity); 
+		  System.out.println("Record saved");
 	}
-
 }
