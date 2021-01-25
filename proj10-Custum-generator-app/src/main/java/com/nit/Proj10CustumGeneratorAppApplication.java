5799b749ae2be2df23471c84fdafaa27795f08d0
vikas yadav <vikas626639@gmail.com>
2021-01-26 03:29:46 +0530

    working with custom generator

---
+++ proj10-Custum-generator-app/src/main/java/com/nit/Proj10CustumGeneratorAppApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj10CustumGeneratorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj10CustumGeneratorAppApplication.class, args);
+	}
+
+}
